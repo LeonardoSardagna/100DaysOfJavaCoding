4ab6e4de652625fd3c8c5cbbc7e7d3d11f03da80
Leonardo Sardagna <leonardosardagna00@gmail.com>
2024-10-23 18:17:32 -0300

    first commit

---
+++ praticando-java/src/main/java/com/praticando/java/PraticandoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.praticando.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticandoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticandoJavaApplication.class, args);
+	}
+
+}
---
+++ praticando-java/src/test/java/com/praticando/java/PraticandoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praticando.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticandoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
